07c28b49d3f3c82ea829c63e3688c8f318d709f9
felixnordgren11 <felix.edu.nordgren@gmail.com>
2023-10-24 11:33:33 +0200

    j

---
+++ atoms.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+
+class Atom:
+    
+    def __init__(self, position):
+        self.position = position
+        self.charge = 1
+        
---
+++ lattice.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class Lattice:
+    
+    def __init__(self, structure):
+        self.structure = structure
+        
+        
